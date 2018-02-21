ba74f68d27675f811548ae71bafd063c11893731
mo <mo@myallocator.com>
2018-02-21 16:57:55 +0000

    doc release

--- api_project.js
+++ api_project.js
@@ -17,7 +17,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-02-15T18:06:28.817Z",
+    "time": "2018-02-21T16:57:55.557Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
